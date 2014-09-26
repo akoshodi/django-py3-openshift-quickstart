a8cad9f79d74692f93f9608887c0374991a9af81
Akinwale Oshodi <akoshodi@gmail.com>
2014-09-26 11:57:20 +0100

    Update setup.py
    Reverted to Django 1.6

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ import os
 # Put here required packages or
 # Uncomment one or more lines below in the install_requires section
 # for the specific client drivers/modules your application needs.
-packages = ['Django<=1.7',
+packages = ['pip',
             'static3',  # If you want serve the static files in the same server
             # 'mysql-connector-python',
             # 'pymongo',
             'psycopg2',
             'wagtail',
+            'Django<=1.6',
+            
             
            ]
 
@@ -19,8 +21,8 @@ if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDIS
      packages.append('hiredis')
 
 setup(name='YourAppName', version='1.0',
-      description='OpenShift Python-3.3 / Django-1.7 Community Cartridge based application',
-      author='Your Name', author_email='admin@example.org',
+      description='OpenShift Python-3.3 / Django-1.6 Community Cartridge based application',
+      author='Akinwale Oshodi', author_email='akoshodi@gmail.com',
       url='https://pypi.python.org/pypi',
       install_requires=packages,
      )
